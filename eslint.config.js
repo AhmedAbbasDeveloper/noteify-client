e65685f85f43a50e05b39213691d94bf09e16604
Ahmed Abbas <theabbos@gmail.com>
2024-12-06 19:42:30 -0500

    create .prettierignore and improve .gitignore and eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -41,6 +41,7 @@ export default tseslint.config(
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       'prettier/prettier': 'error',
+      'linebreak-style': 'off',
     },
   },
 );
